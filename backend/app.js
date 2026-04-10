650a6458ee4461b26fa3def2ffb1a05087f1421c
cosmowebsbd <cosmowebsbd@gmail.com>
2026-04-10 03:12:19 -0700

    for deployment

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,12 @@ const app = express()
 
 app.use(helmet())
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://sports-app-frontend.vercel.app',
+    /\.vercel\.app$/
+  ],
   credentials: true,
 }))
 app.use(morgan('dev'))
